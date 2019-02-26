906273265ba0e4ab956691e46ecde015e386a229
splendid-frame-7429 <bsameera@yahoo.com>
2019-02-26 20:10:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -43,17 +43,17 @@ def nyc_pigeon_organizer(data)
           name_value.each do |attr_key, attr_value|
             if attribute_key == :color && attr_key == :color
               if x == name_key
-                pigeon_list[name_key][:color] << prop_key
+                pigeon_list[name_key][:color] << prop_key.to_s
               end 
             end
             if attribute_key == :gender && attr_key == :gender
               if x == name_key
-                pigeon_list[name_key][:gender] << prop_key
+                pigeon_list[name_key][:gender] << prop_key.to_s
               end 
             end
             if attribute_key == :lives && attr_key == :lives
               if x == name_key
-                pigeon_list[name_key][:lives] << prop_key
+                pigeon_list[name_key][:lives] << prop_key.to_s
               end 
             end
           end 
